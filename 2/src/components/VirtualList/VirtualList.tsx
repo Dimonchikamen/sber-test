b73b3d69dc42b306a92944016f450bbc33860eb8
Dmitrii Chechikhin <65847130+Dimonchikamen@users.noreply.github.com>
2023-08-23 19:04:33 +0500

    feat(VirtualList): added optimization

--- 2/src/components/VirtualList/VirtualList.tsx
+++ 2/src/components/VirtualList/VirtualList.tsx
@@ -5,6 +5,7 @@ import React, {
     CSSProperties,
     useState,
 } from "react";
+import { useDebounce } from "../../helpers/useDebounce";
 
 import s from "./VirtualList.module.css";
 
@@ -25,52 +26,74 @@ type VirtualListProps = {
     renderBuffer?: number;
 };
 
-export const VirtualList = memo(({
-    itemRender,
-    itemCount,
-    itemHeight = 60,
-    height = 250,
-    width = 100,
-    renderBuffer = 0,
-}: VirtualListProps) => {
-    const indexDelta = Math.ceil(height / itemHeight);
-    const [startRenderIndex, setStartRenderIndex] = useState(0);
-    const [endRenderIndex, setEndRenderIndex] = useState(indexDelta + renderBuffer);
-    const listHeight = itemHeight * itemCount;
+export const VirtualList = memo(
+    ({
+        itemRender,
+        itemCount,
+        itemHeight = 60,
+        height = 250,
+        width = 100,
+        renderBuffer = 0,
+    }: VirtualListProps) => {
+        const indexDelta = Math.ceil(height / itemHeight);
+        const [startRenderIndex, setStartRenderIndex] = useState(0);
+        const [endRenderIndex, setEndRenderIndex] = useState(
+            indexDelta + renderBuffer
+        );
+        const debouncedStartRenderIndex = useDebounce(startRenderIndex, 150);
+        const debouncedEndRenderIndex = useDebounce(endRenderIndex, 150);
+        const listHeight = itemHeight * itemCount;
 
-    const scrollHandler = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
-        //@ts-ignore
-        const { scrollTop } = e.target;
-        const previousElementsCount = Math.ceil(scrollTop / itemHeight);
-        const newStartIndex = Math.max(0, previousElementsCount - 1 - renderBuffer);
-        const newEndIndex = Math.min(previousElementsCount + indexDelta + renderBuffer, itemCount);
-        setStartRenderIndex(newStartIndex);
-        setEndRenderIndex(newEndIndex);
-    }, [itemCount, itemHeight, indexDelta, renderBuffer]);
+        const scrollHandler = useCallback(
+            (e: React.MouseEvent<HTMLDivElement>) => {
+                //@ts-ignore
+                const { scrollTop } = e.target;
+                const previousElementsCount = Math.ceil(scrollTop / itemHeight);
+                const newStartIndex = Math.max(
+                    0,
+                    previousElementsCount - 1 - renderBuffer
+                );
+                const newEndIndex = Math.min(
+                    previousElementsCount + indexDelta + renderBuffer,
+                    itemCount
+                );
+                setStartRenderIndex(newStartIndex);
+                setEndRenderIndex(newEndIndex);
+            },
+            [itemCount, itemHeight, indexDelta, renderBuffer]
+        );
 
-    const renderItems = () => {
-        const result = [];
-        for (let index = startRenderIndex; index < endRenderIndex; index++) {
-            result.push(
-                itemRender({
-                    index,
-                    key: `item-${index}`,
-                    style: { height: itemHeight, position: "absolute", top: index * itemHeight },
-                })
-            );
-        }
-        return result;
-    };
+        const renderItems = () => {
+            const result = [];
+            for (let i = debouncedStartRenderIndex; i < debouncedEndRenderIndex; i++) {
+                result.push(
+                    itemRender({
+                        index: i,
+                        key: `item-${i}`,
+                        style: {
+                            height: itemHeight,
+                            position: "absolute",
+                            top: i * itemHeight,
+                        },
+                    })
+                );
+            }
+            return result;
+        };
 
-    return (
-        <div
-            className={s.container}
-            style={{ height, width }}
-            onScroll={scrollHandler}
-        >
-            <ul className={s.list} style={{ height: listHeight, maxHeight: listHeight }}>
-                {renderItems()}
-            </ul>
-        </div>
-    );
-});
+        return (
+            <div
+                className={s.container}
+                style={{ height, width }}
+                onScroll={scrollHandler}
+            >
+                <ul
+                    className={s.list}
+                    style={{ height: listHeight, maxHeight: listHeight }}
+                >
+                    {renderItems()}
+                </ul>
+            </div>
+        );
+    }
+);
